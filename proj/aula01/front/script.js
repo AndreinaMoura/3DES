8b5a38849f3c7c2f47de5dd28555f8b31644955a
Andreina Moura <98525153+AndreinaMoura@users.noreply.github.com>
2023-02-06 11:27:14 -0300

    projeto

--- proj/aula01/front/script.js
+++ proj/aula01/front/script.js
@@ -5,17 +5,19 @@ function carregar() {
         })
         .then((data) => {
             data.forEach(cada => {
-                let select = document.querySelector(".coisa").value
+                // let select = document.querySelector(".coisa").value
 
-                let opn = select.cloneNode(true)
+                // let opn = select.cloneNode(true)
 
+                let selecao = document.querySelector('option')
+                console.log(selecao)
                 // opn.classList.remove("modelo")
+                let nselecao = selecao.innerHTML()
 
                 // let op = opn.querySelector(".produto")
 
-                opn.innerHTML += ` <option value="${cada.produto}">${cada.produto}</option>`;
 
-                document.querySelector(".input").appendChild(opn);
+                document.querySelector("select").appendChild(nselecao);
             })
         })
 }
