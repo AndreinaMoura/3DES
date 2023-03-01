540be734c5ffb876f2840c774bf5b8799c633137
Andreina Moura <98525153+AndreinaMoura@users.noreply.github.com>
2023-03-01 11:17:33 -0300

    projeto

--- proj/aula06/mobile/projeto/App.js
+++ proj/aula06/mobile/projeto/App.js
@@ -1,20 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+
+import Login from './src/pages/login/login';
+import Home from './src/pages/home/home';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Login/>
+    <Home/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
